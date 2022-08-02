f871b311da886a61872df58cd0d21d340d44822c
Paulo <ph744915@gmail.com>
2022-08-02 11:14:26 -0300

    formato mvc

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # escritorio
 Estou criando um sistema para facilitar o meu serviço atual, onde consulto muito um site repetidamente.
+
+
+~~~
+php 8
+composer
+xampp
+~~~
---
+++ lib/composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wef/calendar',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2592272b23426bf5d389be5993effa7ef8ffdb35',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wef/calendar' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2592272b23426bf5d389be5993effa7ef8ffdb35',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
