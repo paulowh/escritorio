2592272b23426bf5d389be5993effa7ef8ffdb35
Paulo <ph744915@gmail.com>
2022-08-02 10:49:50 -0300

    'r'

--- categoria-editar-post.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'Classes/Categoria.php';
-    $id = $_POST['id'];
-    $descricao = $_POST['descricao'];
-    $categoria = new Categoria();
-    $categoria->alterar($id, $descricao);
-
-    header('Location:categorias.php');
-?>
--- rodape.php
+++
@@ -1,7 +0,0 @@
-<hr>
-<footer>
-    <p>&copy; 2019 Escola Estadual Mariuma - &copy; Paulo Henrique</p>
-</footer>
-</div>
-</body>
-</html>
